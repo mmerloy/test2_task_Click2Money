b890409ca70f3836119843fec364267b5644f7e9
mmerloy <a.mmerloy@gmail.com>
2022-08-04 20:03:14 +0300

    added class MainPage

---
+++ src/test/java/MainPage.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+/**
+ * Главная страница(поисковая система google)
+ */
+public class MainPage
+{
+    //поле для ввода поискового запроса
+    private final SelenideElement textBoxInput = $x("//input[@type='text']");
+
+    public MainPage(String url){
+        Selenide.open(url);
+    }
+
+
+    /**
+     * Выполняется поиск в поисковой системе необходимой картинки и нажимается кнопка Enter
+     * @param searchString поисковая строка
+     */
+    protected SearchPage search(String searchString)
+    {
+        textBoxInput.setValue(searchString);
+        textBoxInput.sendKeys(Keys.ENTER);
+        return new SearchPage();
+    }
+}
