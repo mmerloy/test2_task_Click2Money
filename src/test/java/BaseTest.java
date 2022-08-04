f9da7f8b2d84f556cc09f3b40a78ebc7c4bbdd11
mmerloy <a.mmerloy@gmail.com>
2022-08-04 16:53:21 +0300

    added BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,51 @@
-public class BaseTest {
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
 
+
+abstract public class BaseTest
+{
+
+//    public enum BrowserType {
+//        Chroum,
+//        Firefox,
+//    }
+
+    public void setUp()//add javaDoc and comments
+    {
+       // chooseBrowser();?
+        WebDriverManager.chromedriver().setup();
+        Configuration.browser = "chrome";
+        Configuration.driverManagerEnabled = true;
+        Configuration.browserSize = "1600x900";
+        Configuration.headless = true;
+    }
+
+//    protected void chooseBrowser(BrowserType browser)
+//    {
+//        switch (browser) {
+//            case Chroum -> {
+//                WebDriverManager.chromedriver().setup();
+//                Configuration.browser = "chrome";
+//            }
+//            case Firefox -> {
+//                WebDriverManager.firefoxdriver().setup();
+//                Configuration.browser = "firefox";
+//            }
+//        }
+//    }
+
+    @Before
+    public void init()
+    {
+        setUp();
+    }
+
+    @After
+    public void tearDown()
+    {
+        Selenide.closeWebDriver();
+    }
 }
