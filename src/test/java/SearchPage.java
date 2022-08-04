68392aa29213fe70aa2673f34131737d2bffa6ad
mmerloy <a.mmerloy@gmail.com>
2022-08-04 23:56:14 +0300

    adding changes to edit the number of the picture to download

--- src/test/java/SearchPage.java
+++ src/test/java/SearchPage.java
@@ -4,6 +4,7 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.protocol.BasicHttpContext;
+import org.openqa.selenium.support.FindBy;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,6 +17,8 @@ import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 * */
 public class SearchPage {
     private final SelenideElement imagesTab = $x("//*[@id=\"hdtb-msb\"]/div[1]/div/div[2]/a");
+    //нахождение 3 картинки по умолчанию
+    //private final SelenideElement imageNum = $x("//*[@id=\"islrg\"]/div[1]/div[3]/a[1]");
     private final SelenideElement thirdImage = $x("//*[@id=\"islrg\"]/div[1]/div[3]/a[1]");
     private final SelenideElement myImage = $x("//*[@id=\"Sva75c\"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[3]/div/a/img");
     private final String filePath = new File(".").getAbsolutePath() + "\\src\\result-image";
@@ -28,6 +31,23 @@ public class SearchPage {
         imagesTab.click();
         return this;
     }
+
+    /**
+     * Метод для замены номера картинки для скачивания
+     * @return новый XPath для нахождения картинки
+     */
+    public String changeCharInPosition(int position, char ch, String str)
+    {
+        char[] charArray = str.toCharArray();
+        charArray[position] = ch;
+        return new String(charArray);
+    }
+
+//    public void chooseImage()
+//    {
+//        String str = String.valueOf(imageNum);
+//        str = changeCharInPosition();
+//    }
 
     /**
      * Комментарий на будущее: создать метод, который создает нужный XPath для выбора различного номера картинки
