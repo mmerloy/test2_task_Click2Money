883785b04b37671c5afb0fbfe4b58be4322ae32c
mmerloy <a.mmerloy@gmail.com>
2022-08-05 00:35:26 +0300

    done

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 
+/**
+ * @author mmerloy
+ * @version 0.0.1
+ * @since 04.08.2022 23:59
+ */
 abstract public class BaseTest
 {
     public enum BrowserType {
